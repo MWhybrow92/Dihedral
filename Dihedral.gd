767ef093236ec57c14eec191d2c2b5b340a7dc46
MWhybrow92 <mlw10@ic.ac.uk>
2019-02-21 11:15:17 +0100

    Changed main files to install/declare global funcs

---
+++ Dihedral.gd
@@ -0,0 +1,37 @@
+DeclareGlobalFunction( "SparseVandermondeMat" );
+
+DeclareGlobalFunction( "FindFirstEigenvectors" );
+
+DeclareGlobalFunction( "FindFlip" );
+
+DeclareGlobalFunction( "DihedralAlgebrasFlip" );
+
+DeclareGlobalFunction( "DihedralAlgebrasRemoveNullVec" );
+
+DeclareGlobalFunction( "DihedralAlgebrasChangeRing" );
+
+DeclareGlobalFunction( "DihedralAlgebrasSetup" );
+
+DeclareGlobalFunction( "DihedralAlgebrasExpand" );
+
+DeclareGlobalFunction( "DihedralAlgebrasFlipPolynomial" );
+
+DeclareGlobalFunction( "DihedralAlgebrasFlipVector" );
+
+DeclareGlobalFunction( "MAJORANA_NaiveSeparateAlgebraProduct" );
+
+DeclareGlobalFunction( "DihedralAlgebrasSolutionProducts" );
+
+DeclareGlobalFunction( "DihedralAlgebrasEigenvectorsUnknowns" );
+
+DeclareGlobalFunction( "DihedralAlgebrasFindNullVecs" );
+
+DeclareGlobalFunction( "DihedralAlgebrasFusion" );
+
+DeclareGlobalFunction( "DihedralAlgebrasIntersectEigenspaces" );
+
+DeclareGlobalFunction( "DihedralAlgebrasMainLoop" );
+
+DeclareGlobalFunction( "DihedralAlgebras" );
+
+DeclareGlobalFunction( "JordanTable" );
